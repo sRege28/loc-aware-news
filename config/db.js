dece1350ab0e89e6091751faa5b1feb379fd63c0
Sudeep Rege <Sudeep Rege>
2017-01-18 12:36:56 -0500

    Fixed db names in config

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 // config/db.js
 module.exports = {
 
-    //url : 'mongodb://admin:group13@ds145677.mlab.com:45677/newsdb',
-    url : 'mongodb://localhost:27017/newsdb',
-    news_col : 'finalnews',
-    twitter_col : 'finaltwitter'
+    url : 'mongodb://admin:group13@ds145677.mlab.com:45677/newsdb',
+    //url : 'mongodb://localhost:27017/newsdb',
+    news_col : 'news',
+    twitter_col : 'twitter'
 }
 
